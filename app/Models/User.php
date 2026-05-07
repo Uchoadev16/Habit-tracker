b275b7574d6fc0efdafd9ec890a574162fd9be04
Pedro Uchôa de Abreu <uchoadev16@gmail.com>
2026-05-07 10:43:25 -0300

    criando relacionamentos

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 #[Fillable(['name', 'email', 'password'])]
@@ -25,5 +26,15 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+
+    //o usuario pode ter varios habitos
+    public function habits(): HasMany{
+        return $this->hasMany(Habit::class);
+    }
+
+    //o usuario pode ter varios logs de habitos
+    public function habitsLogs(): HasMany{
+        return $this->hasMany(HabitLog::class);
     }
 }
